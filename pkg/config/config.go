cbec8354accc8d3accaf2bea8684753ce3d6dc0c
Fachrin Aulia <fachrin.nasution@tokopedia.com>
2020-02-02 23:22:51 +0700

    i fachrin redefine app config #2

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,6 +1,10 @@
 package config
 
 import (
+	"sync"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/labstack/echo"
 	"github.com/spf13/viper"
 )
 
@@ -10,20 +14,38 @@ const (
 	configPath = "/etc/purwalenta/"
 )
 
-func GetConfig() Config {
-	var cfg Config
+var (
+	once           sync.Once
+	configInstance = new(Config)
+)
 
-	viper.SetConfigName(configName)
-	viper.SetConfigType(configType)
-	viper.AddConfigPath(configPath)
+func GetConfig(ctx echo.Context) *Config {
+	once.Do(func() {
+		viper.SetConfigName(configName)
+		viper.SetConfigType(configType)
+		viper.AddConfigPath(configPath)
 
-	if err := viper.ReadInConfig(); nil != err {
-		return cfg
-	}
+		var unpackToStruct = func(ctx echo.Context, cfgStruct *Config) {
+			if err := viper.Unmarshal(&cfgStruct); nil != err {
+				ctx.Logger().Error(err)
+			}
+		}
 
-	if err := viper.Unmarshal(&cfg); nil != err {
-		return cfg
-	}
+		if err := viper.ReadInConfig(); nil != err {
+			ctx.Logger().Error(err)
+			return
+		}
 
-	return cfg
+		// if config is successfully read; then unpack it to struct
+		unpackToStruct(ctx, configInstance)
+
+		go func() {
+			viper.WatchConfig()
+			viper.OnConfigChange(func(e fsnotify.Event) {
+				unpackToStruct(ctx, configInstance)
+			})
+		}()
+	})
+
+	return configInstance
 }
